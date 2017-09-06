e91f6966e69dbcf3b65b79de1cf17dd9f3f9c92a
SalVN <sal.v.newell@gmail.com>
2017-09-06 18:08:27 +0100

    create TopicNavBar rendering with hard-coded data

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
+import TopicNavBar from './TopicNavBar';
+
 class App extends React.Component {
   render () {
     return (
       <div>
+        <TopicNavBar />
         <h3 className='title is-3'>All Articles</h3>
         {this.props.children}
       </div>
---
+++ src/components/TopicNavBar.js
@@ -0,0 +1,48 @@
+import React, { Component } from 'react';
+import { BrowserRouter as NavLink } from 'react-router-dom';
+
+const topics = [
+    {
+        '_id': '583412915905f02e4c8e6dfd',
+        'title': 'Football',
+        'slug': 'football',
+        '__v': 0
+    },
+    {
+        '_id': '583412915905f02e4c8e6dfe',
+        'title': 'Cooking',
+        'slug': 'cooking',
+        '__v': 0
+    },
+    {
+        '_id': '583412915905f02e4c8e6dff',
+        'title': 'Coding',
+        'slug': 'coding',
+        '__v': 0
+    }
+];
+
+class TopicNavBar extends Component {
+    render () {
+        return (
+            <nav className='navbar'>
+                <div className='navbar-start'>
+                    {topics.map((topic, i) => {
+                        return (
+                            <span className='navbar-item'>
+                                <NavLink to='#'>
+                                    <span>{`< ${topic.title.toUpperCase()} />`}</span>
+                                </NavLink>
+                                {i !== topics.length - 1 &&
+                                    <span className='navbar-item'>&nbsp;&nbsp;</span>
+                                }
+                            </span>
+                        );
+                    })}
+                </div>
+            </nav>
+        );
+    }
+}
+
+export default TopicNavBar;
