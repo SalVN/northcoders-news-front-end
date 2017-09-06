7ad5d349c73f6003aafee7bb94e49b21e4aa14ce
SalVN <sal.v.newell@gmail.com>
2017-09-06 19:31:42 +0100

    make navbar toggleable and responsive

--- src/components/TopicNavBar.js
+++ src/components/TopicNavBar.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { BrowserRouter as Router, NavLink } from 'react-router-dom';
+import { BrowserRouter as Router, NavLink, Link } from 'react-router-dom';
 import './css/TopicNavBar.css';
 
 const topics = [
@@ -24,25 +24,53 @@ const topics = [
 ];
 
 class TopicNavBar extends Component {
-    render () {
+    constructor (props) {
+        super (props);
+        this.state = {
+            activeBurger: false
+        };
+        this.toggleBurger = this.toggleBurger.bind(this);
+    }
+    render() {
+        let activeBurger = (this.state.activeBurger) ? 'is-active' : '';
         return (
             <nav className='navbar navbar-css'>
-                <div className='navbar-start'>
-                    {topics.map((topic, i) => {
-                        return (
-                            <span className='navbar-item'>
-                                <NavLink className='navbar-text-css' to='#'>
-                                    <span>{`< ${topic.title.toUpperCase()} />`}</span>
-                                </NavLink>
-                                {i !== topics.length - 1 &&
-                                    <span className='navbar-item'>&nbsp;&nbsp;</span>
-                                }
-                            </span>
-                        );
-                    })}
+                <div className='navbar-brand'>
+                    <span className='navbar-item' >
+                        <Link to='#'>
+                            <img className='logo' src='https://avatars3.githubusercontent.com/u/6791502?v=3&s=200' alt='Northcoders logo' />
+                        </Link>
+                    </span>
+                    <div onClick={this.toggleBurger} className={`navbar-burger ${activeBurger}`}>
+                        <div className="navbar-burger">
+                            <span></span>
+                            <span></span>
+                            <span></span>
+                        </div>
+                    </div>
                 </div>
-            </nav>
+                <div className={`navbar-menu ${activeBurger}`}>
+                    <div className='navbar-start'>
+                        {topics.map((topic, i) => {
+                            return (
+                                <span className='navbar-item is-hoverable'>
+                                    <NavLink className='navbar-text-css' to='#'>
+                                        <span>{`< ${topic.title.toUpperCase()} />`}</span>
+                                    </NavLink>
+                                </span>
+                            );
+                        })}
+                    </div>
+                    <div className='navbar-end'>
+                    </div>
+                </div>
+            </nav >
         );
+    }
+    toggleBurger () {
+        this.setState({
+            activeBurger: !this.state.activeBurger
+        });
     }
 }
 
