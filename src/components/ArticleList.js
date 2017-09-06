8a7284f3409a6008b5ba313bc705bc1a2cdb0fcb
SalVN <sal.v.newell@gmail.com>
2017-09-06 17:34:16 +0100

    render ArticleList using state and reducer

--- src/components/ArticleList.js
+++ src/components/ArticleList.js
@@ -1,18 +1,44 @@
 import React from 'react';
 import ArticleCard from './ArticleCard';
+import PropTypes from 'prop-types';
+import {connect} from 'react-redux';
 
-const articles = [{ title: 'I\'m an article', votes: 4 },
-{ title: 'I\'m another article', votes: 5 },
-{ title: 'I\'m an article too', votes: 2 }];
+import * as actions from '../actions/actions';
 
 class ArticleList extends React.Component {
+  componentDidMount () {
+    this.props.fetchArticles();
+  }
   render () {
     return (
       <div id='ArticleList'>
-        {articles.map(article => <ArticleCard title={article.title} votes={article.votes} key={article.title} />)}
+        { this.props.articles.length > 0 &&
+          this.props.articles.map(article => <ArticleCard title={article.title} votes={article.votes} key={article.title} />)}
       </div>
     );
   }
 }
 
-export default ArticleList;
+function mapDispatchToProps(dispatch) {
+  return {
+    fetchArticles: () => {
+      dispatch(actions.fetchArticles());
+    }
+  };
+}
+
+function mapStateToProps (state) {
+  console.log(state);
+  return {
+    articles: state.articles,
+    loading: state.loading
+  };
+}
+
+ArticleList.propTypes = {
+  articles: PropTypes.array.isRequired,
+  loading: PropTypes.bool.isRequired,
+  fetchArticles: PropTypes.func.isRequired
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(ArticleList);
--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { createStore } from 'redux';
 import { Provider } from 'react-redux';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import createBrowserHistory from 'history/createBrowserHistory';
+import thunk from 'redux-thunk';
+import logger from 'redux-logger';
+import {createStore, applyMiddleware} from 'redux';
 import './css/bulma.css';
 import './css/font-awesome.css';
 
@@ -11,7 +13,7 @@ import App from './components/App';
 import ArticleList from './components/ArticleList';
 import reducer from './reducer/reducer';
 
-const store = createStore(reducer);
+const store = createStore(reducer, applyMiddleware(thunk, logger));
 const history = createBrowserHistory();
 
 ReactDOM.render(<Provider store={store}>
