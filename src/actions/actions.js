c3e193a767ca92aa9747265c236a7022bc773ec2
SalVN <sal.v.newell@gmail.com>
2017-09-06 15:15:17 +0100

    [FETCH_ALL_ARTICLES] create the action file

--- src/actions/actions.js
+++ src/actions/actions.js
@@ -1,1 +1,37 @@
-// import * as types from './types';
+import axios from 'axios';
+import {ROOT} from '../../config';
+
+import * as types from './types';
+
+export function fetchArticles () {
+    return function (dispatch) {
+        dispatch(fetchArticlesRequest());
+        axios.get(`${ROOT}/articles`)
+        .then(res => {
+            dispatch(fetchArticlesSuccess(res.data.articles));
+        })
+        .catch(err => {
+            dispatch(fetchArticlesError(err));
+        });
+    };
+}
+
+export function fetchArticlesRequest () {
+    return {
+        type: types.FETCH_ARTICLES_REQUEST
+    };
+}
+
+export function fetchArticlesSuccess (articles) {
+    return {
+        type: types.FETCH_ARTICLES_SUCCESS,
+        data: articles
+    };
+}
+
+export function fetchArticlesError (err) {
+    return {
+        type: types.FETCH_ARTICLES_ERROR,
+        data: err
+    };
+}
