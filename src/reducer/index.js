e317af6b4cf64d7f0b52d1c81f4a145d3a20725d
SalVN <sal.v.newell@gmail.com>
2017-09-07 21:20:26 +0100

    FETCH_USERS types

--- src/reducer/index.js
+++ src/reducer/index.js
@@ -3,10 +3,12 @@ import articlesReducer from './articles.reducer.js';
 import topicsReducer from './topics.reducer.js';
 import commentsReducer from './comments.reducer.js';
 import topicArticlesReducer from './topicArticles.reducer.js';
+import usersReducer from './usersReducer.reducer.js';
 
 export default combineReducers({
     articles: articlesReducer,
     topics: topicsReducer,
     comments: commentsReducer,
-    topicArticles: topicArticlesReducer
+    topicArticles: topicArticlesReducer,
+    users: usersReducer
 });
