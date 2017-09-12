47ba656484d9b27039be24a3d6af0d88990634db
SalVN <sal.v.newell@gmail.com>
2017-09-12 13:31:44 +0100

    change toggle method for UserListCard dropdown

--- src/components/UserListCard.js
+++ src/components/UserListCard.js
@@ -28,6 +28,7 @@ class UserListCard extends Component {
         if (this.props.users && this.props.users.length > 0) {
             sortUsers(this.props.users, this.state.sortBy);
         }
+        let activeDropdown = this.state.showDropdown ? 'is-active' : '';
         return (
             <div className='card-box'>
                 <div className='container-fluid'>
@@ -35,7 +36,7 @@ class UserListCard extends Component {
                         <header className='card-header'>
                             <div className='card-header-style'>
                                 <p className='card-header-title'>
-                                    <div className="dropdown is-active">
+                                    <div className={`dropdown ${activeDropdown}`}>
                                         <div className="dropdown-trigger">
                                             <button onClick={this.toggleDropdown} className="button is-small is-primary is-inverted is-outlined dropdown-button" aria-haspopup="true" aria-controls="dropdown-menu">
                                                 <span className="icon">
@@ -43,27 +44,25 @@ class UserListCard extends Component {
                                                 </span>
                                             </button>
                                         </div>
-                                        {this.state.showDropdown &&
-                                            <div className="dropdown-menu" id="dropdown-menu" role="menu">
-                                                <div className="dropdown-content">
-                                                    <a onClick={this.sortUserList} className="dropdown-item">
-                                                        Sort by ranking
-                                                    </a>
-                                                    <a onClick={this.sortUserList} className="dropdown-item">
-                                                        Sort by comments
-                                                    </a>
-                                                    <a onClick={this.sortUserList} className="dropdown-item">
-                                                        Sort by comment votes
-                                                    </a>
-                                                    <a onClick={this.sortUserList} className="dropdown-item">
-                                                        Sort by article votes
-                                                    </a>
-                                                    <a onClick={this.toggleDropdown} className="dropdown-item cancel">
-                                                        Close
-                                                    </a>
-                                                </div>
+                                        <div className="dropdown-menu" id="dropdown-menu" role="menu">
+                                            <div className="dropdown-content">
+                                                <a onClick={this.sortUserList} className="dropdown-item">
+                                                    Sort by ranking
+                                                </a>
+                                                <a onClick={this.sortUserList} className="dropdown-item">
+                                                    Sort by comments
+                                                </a>
+                                                <a onClick={this.sortUserList} className="dropdown-item">
+                                                    Sort by comment votes
+                                                </a>
+                                                <a onClick={this.sortUserList} className="dropdown-item">
+                                                    Sort by article votes
+                                                </a>
+                                                <a onClick={this.toggleDropdown} className="dropdown-item cancel">
+                                                    Close
+                                                </a>
                                             </div>
-                                        }
+                                        </div>
                                     </div>
                                     <span>User Ranking</span>
                                 </p>
