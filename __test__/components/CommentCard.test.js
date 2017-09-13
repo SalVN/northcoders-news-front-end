fabad2134e1f701dcc48e5572b1c6b17e9fb1835
SalVN <sal.v.newell@gmail.com>
2017-09-13 22:17:12 +0100

    add additional component testing for CommentCard

--- __test__/components/CommentCard.test.js
+++ __test__/components/CommentCard.test.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { shallow } from 'enzyme';
 import renderer from 'react-test-renderer';
 import { MemoryRouter } from 'react-router-dom';
+import sinon from 'sinon';
 
 import CommentCard from '../../src/components/CommentCard';
 
@@ -15,6 +16,25 @@ describe('CommentCard', () => {
         votes: 1,
         created_at: 1504778965845
     };
+
+    const otherUserComment = {
+        _id: '59b11ae18807841d9bf13234',
+        body: 'this is a comment',
+        belongs_to: '59b11ae18807841d9bf13232',
+        __v: 0,
+        created_by: 'not_northcoder',
+        votes: 1,
+        created_at: 1504778965845
+    };
+
+    const user = {
+        _id: '59b1b18b327cce1fb043bdb1',
+        username: 'northcoder',
+        name: 'Awesome Northcoder',
+        avatar_url: 'https://avatars3.githubusercontent.com/u/6791502?v=3&s=200',
+        __v: 0
+    };
+
     it('is a function', () => {
         expect(typeof CommentCard).toEqual('function');
     });
@@ -24,6 +44,7 @@ describe('CommentCard', () => {
             voteHandler={(x) => { return x; }}
             deleteHandler={(x) => { return x; }}
             comment={comment}
+            userData={user}
         />);
         expect(enzymeWrapper.children().length).toEqual(1);
     });
@@ -32,12 +53,82 @@ describe('CommentCard', () => {
         const tree = renderer.create(
             <MemoryRouter>
                 <CommentCard
-                    voteHandler={(x) => { return x; }}
-                    deleteHandler={(x) => { return x; }}
+                    voteHandler={x => x}
+                    deleteHandler={x => x}
                     comment={comment}
+                    userData={user}
                 />
             </MemoryRouter>
         ).toJSON();
         expect(tree).toMatchSnapshot();
+    });
+
+    it('contains one link', () => {
+        const enzymeWrapper = shallow(<CommentCard
+            voteHandler={x => x}
+            deleteHandler={x => x}
+            comment={comment}
+            userData={user}
+        />);
+        expect(enzymeWrapper.find('Link').length).toBe(1);
+    });
+
+    it('renders a delete button if the comment was added by the user', () => {
+        const enzymeWrapper = shallow(<CommentCard
+            voteHandler={(x) => { return x; }}
+            deleteHandler={(x) => { return x; }}
+            comment={comment}
+            userData={user}
+        />);
+        expect(enzymeWrapper.find('.delete-button').length).toEqual(1);
+    });
+
+    it('does not render a delete button if the comment was added by a different user', () => {
+        const enzymeWrapper = shallow(<CommentCard
+            voteHandler={(x) => { return x; }}
+            deleteHandler={(x) => { return x; }}
+            comment={otherUserComment}
+            userData={user}
+        />);
+        expect(enzymeWrapper.find('.delete-button').length).toEqual(0);
+    });
+
+    it('calls the voteHandler function when the "up" button is clicked', () => {
+        const spy = sinon.stub();
+        const enzymeWrapper = shallow(<CommentCard
+            voteHandler={spy}
+            deleteHandler={(x) => { return x; }}
+            comment={comment}
+            userData={user}
+        />);
+        expect(spy.called).toBe(false);
+        enzymeWrapper.find('.up').simulate('click', { preventDefault() { } });
+        expect(spy.called).toBe(true);
+    });
+
+    it('calls the voteHandler function when the "down" button is clicked', () => {
+        const spy = sinon.stub();
+        const enzymeWrapper = shallow(<CommentCard
+            voteHandler={spy}
+            deleteHandler={(x) => { return x; }}
+            comment={comment}
+            userData={user}
+        />);
+        expect(spy.called).toBe(false);
+        enzymeWrapper.find('.down').simulate('click', { preventDefault() { } });
+        expect(spy.called).toBe(true);
+    });
+
+    it('calls the deleteHandler function when the "delete" button is clicked', () => {
+        const spy = sinon.stub();
+        const enzymeWrapper = shallow(<CommentCard
+            voteHandler={x => x}
+            deleteHandler={spy}
+            comment={comment}
+            userData={user}
+        />);
+        expect(spy.called).toBe(false);
+        enzymeWrapper.find('.delete-button').simulate('click', { preventDefault() { } });
+        expect(spy.called).toBe(true);
     });
 });
--- src/components/CommentCard.js
+++ src/components/CommentCard.js
@@ -12,20 +12,17 @@ const CommentCard = function (props) {
     <div className='card box-style'>
       <article className='media'>
         <div className='media-left'>
-          <button onClick={props.voteHandler.bind(this, 'up', props.comment._id)} className='button is-black is-inverted is-small'><i className="fa fa-arrow-up" aria-hidden="true"></i></button>
+          <button onClick={props.voteHandler.bind(this, 'up', props.comment._id)} className='button is-black is-inverted is-small up'><i className="fa fa-arrow-up" aria-hidden="true"></i></button>
           <p className='comment-card-votes'><strong>{props.comment.votes}</strong></p>
-          <button onClick={props.voteHandler.bind(this, 'down', props.comment._id)} className='button is-black is-inverted is-small'><i className="fa fa-arrow-down" aria-hidden="true"></i></button>
+          <button onClick={props.voteHandler.bind(this, 'down', props.comment._id)} className='button is-black is-inverted is-small down'><i className="fa fa-arrow-down" aria-hidden="true"></i></button>
         </div>
         <div className='media-content'>
           <div className='content'>
-
-            {props.userData &&
               <div>
                 <img className='comment-card-avatar' src={props.userData.avatar_url} alt='user avatar' />
                 <Link to={`/users/${props.userData.username}`}><span className='comment-card-name'><strong>{props.comment.created_by}</strong></span></Link>
                 <p className='comment-card-date'>{moment(props.comment.created_at).utc().format('Do MMMM YYYY, hh:mm')}</p>
               </div>
-            }
             <div className='comment-card-body'>{props.comment.body}</div>
             {props.comment.created_by === USERNAME &&
               <button onClick={props.deleteHandler.bind(this, props.comment._id)} className='button is-small delete-button'>Delete Comment</button>
@@ -41,7 +38,7 @@ CommentCard.propTypes = {
   comment: PropTypes.object.isRequired,
   deleteHandler: PropTypes.func.isRequired,
   voteHandler: PropTypes.func.isRequired,
-  userData: PropTypes.object
+  userData: PropTypes.object.isRequired
 };
 
 
